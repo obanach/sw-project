d1f52bb6e24fd7975137ded52c7b0451a312bbf6
Oliwer Banach <oliwerbanach@gmail.com>
2023-01-14 20:52:37 +0100

    added frontend

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import json
 import tinytuya
-from flask import Flask, abort
+from flask_bootstrap import Bootstrap5
+from flask import Flask, abort, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+bootstrap = Bootstrap5(app)
+
 lightBulb = tinytuya.OutletDevice('bf6fe6278bc779da94d49v', '192.168.18.8', 'a5e0fef167323362', version=3.4)
 windowRight = tinytuya.CoverDevice('bf1bcf4ed0e156e13bsbvz', '192.168.18.6', 'c6db31d029839f98', version=3.3)
 windowLeft = tinytuya.CoverDevice('bf013c91d887adce65taat', '192.168.18.7', '651823889d72de81', version=3.3)
 
 if __name__ == '__main__':
     app.run()
+
+
+@app.route('/')
+def dashboard():
+    return render_template('index.html')
 
 
 # Light requests
@@ -20,13 +30,23 @@ def light_set(status):
             'success': True,
             'message': 'Light on'
         })
-
-    else:
+    elif status == 0:
         lightBulb.turn_off()
         return json.dumps({
             'success': True,
             'message': 'Light off'
         })
+    else:
+        return json.dumps({
+            'success': False,
+            'message': 'Invalid status '
+        })
+
+
+@app.route('/light/status')
+def light_status():
+    data = lightBulb.status()
+    return json.dumps({'success': True, 'status': data['dps']['1']})
 
 
 # Window requests
@@ -42,14 +62,12 @@ def window_set(side=None, data=None):
             'success': True,
             'message': 'Stopping window'
         })
-
     elif data == 1:
         side.send(side.generate_payload(tinytuya.CONTROL, {"1": "open"}))
         return json.dumps({
             'success': True,
             'message': 'Opening window'
         })
-
     elif data == 2:
         side.send(side.generate_payload(tinytuya.CONTROL, {"1": "close"}))
         return json.dumps({
@@ -59,7 +77,7 @@ def window_set(side=None, data=None):
     else:
         return json.dumps({
             'success': False,
-            'message': 'Unknown code'
+            'message': 'Invalid status'
         })
 
 
@@ -74,6 +92,32 @@ def window_status(side):
         'success': True,
         'status': data['dps']['1'],
         'value': data['dps']['2'],
+    })
+
+
+@app.route('/status')
+def all_devices_status():
+    lightData = lightBulb.status()['dps']['1']
+    windowLeftData = windowLeft.status()['dps']
+    windowRightData = windowRight.status()['dps']
+
+    if windowLeftData['1'] == 'stop':
+        windowLeftData = windowLeftData['2']
+    else:
+        windowLeftData = windowLeftData['1']
+
+    if windowRightData['1'] == 'stop':
+        windowRightData = windowRightData['2']
+    else:
+        windowRightData = windowRightData['1']
+
+    return json.dumps({
+        'success': True,
+        'data': {
+            'light': lightData,
+            'windowLeft': windowLeftData,
+            'windowRight': windowRightData,
+        }
     })
 
 
